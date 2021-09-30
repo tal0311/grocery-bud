040128a892f75f840608aaa539c6ca9146aba383
tal0311 <tal.amit0311@gmail.com>
2021-09-30 19:06:08 +0300

    changed btn color

--- src/App.js
+++ src/App.js
@@ -69,32 +69,36 @@ function App() {
   }, [list])
 
   return (
-    <section className='section-center'>
-      <form className='grocery-form' onSubmit={handleSubmit}>
-        {alert.show && <Alert {...alert} removeAlert={showAlert} list={list} />}
-        <h3>grocery bud</h3>
-        <div className='form-control'>
-          <input
-            type='text'
-            className='grocery'
-            placeholder='e.g eggs'
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-          <button type='submit' className='submit-btn'>
-            {isEditing ? 'edit' : 'submit'}
-          </button>
-        </div>
-      </form>
-      {list.length > 0 && (
-        <div className='grocery-container'>
-          <List items={list} editItem={editItem} removeItem={removeItem} />
-          <button className='clear-btn' onClick={() => clearList()}>
-            clear items
-          </button>
-        </div>
-      )}
-    </section>
+    <>
+      <section className='section-center'>
+        <form className='grocery-form' onSubmit={handleSubmit}>
+          {alert.show && (
+            <Alert {...alert} removeAlert={showAlert} list={list} />
+          )}
+          <h3>grocery bud</h3>
+          <div className='form-control'>
+            <input
+              type='text'
+              className='grocery'
+              placeholder='e.g eggs'
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <button type='submit' className='submit-btn'>
+              {isEditing ? 'edit' : 'submit'}
+            </button>
+          </div>
+        </form>
+        {list.length > 0 && (
+          <div className='grocery-container'>
+            <List items={list} editItem={editItem} removeItem={removeItem} />
+            <button className='clear-btn' onClick={() => clearList()}>
+              clear items
+            </button>
+          </div>
+        )}
+      </section>
+    </>
   )
 }
 
